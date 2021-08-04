75f3503848f9eec7fdc2742e58ba0a2f1fd6c6a2
dengshuaihui <dengshuaihui@joyy.sg>
2021-08-04 11:01:13 +0800

    feat: init

---
+++ src/components/LImage/LImage.vue
@@ -0,0 +1,36 @@
+<template>
+  <img
+    :style="styleProps"
+    class="l-image-component"
+    :src="src"
+    @click.prevent="handleClick"
+  />
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import {
+  transformToComponentProps,
+  imageDefaultProps,
+  imageStylePropNames,
+} from "@/types/defaultProps";
+import { useComponentCommon } from "@/hooks/index";
+const defaultProps = transformToComponentProps(imageDefaultProps);
+export default defineComponent({
+  name: "l-image",
+  props: {
+    ...defaultProps,
+  },
+  setup(props) {
+    const { styleProps, handleClick } = useComponentCommon(
+      props,
+      imageStylePropNames
+    );
+    return {
+      styleProps,
+      handleClick,
+    };
+  },
+});
+</script>
+<style>
+</style>
---
+++ src/components/LText/LText.vue
@@ -0,0 +1,39 @@
+<template>
+  <component
+    :is="tag"
+    :style="styleProps"
+    class="l-text-component"
+    @click="handleClick"
+    >{{ text }}</component
+  >
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import {
+  transformToComponentProps,
+  textDefaultProps,
+  textStylePropNames,
+} from "@/types/defaultProps";
+import { useComponentCommon } from "@/hooks/index";
+const defaultProps = transformToComponentProps(textDefaultProps);
+export default defineComponent({
+  name: "l-text",
+  props: {
+    ...defaultProps,
+    tag: {
+      type: String,
+      default: "div",
+    },
+  },
+  setup(props) {
+    const { styleProps, handleClick } = useComponentCommon(
+      props,
+      textStylePropNames
+    );
+    return {
+      styleProps,
+      handleClick,
+    };
+  },
+});
+</script>
