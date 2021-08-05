984a190a7fe81bb8709363bb4caa0e53f3d039cc
dengshuaihui <dengshuaihui@joyy.sg>
2021-08-05 11:14:46 +0800

    chore: change pack name

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -3,7 +3,7 @@ import vue from 'rollup-plugin-vue'
 import typescript from 'rollup-plugin-typescript2'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { name } from '../package.json'
-const file = (type) => `dist/${name}.${type}.js`
+const file = (type) => `dist/index.${type}.js`
 export { file, name }
 const overrides = {
   compilerOptions: { declaration: true },
