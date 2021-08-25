f84cb308d528cfe368a0cd31bda7755318ce885c
dengshuaihui <dengshuaihui@joyy.sg>
2021-08-25 16:32:44 +0800

    chore: 添加类型

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
+import type { App } from 'vue'
 import LImage from "./components/LImage";
 import LText from "./components/LText";
 import LShape from "./components/LShape";
-
 const components = [
     LImage,
     LText,
     LShape
 ]
 
-const install = (app: any) => {
+const install = (app: App) => {
     components.forEach(component =>{
     app.component(component.name,component)
     })
